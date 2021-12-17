c958e499e097b9fd136fe6521cb01a62af05d509
Simon Que <simon_justin_que@dlsu.edu.ph>
2021-12-17 17:18:33 +0800

    fied bug in mount script

--- mount_all_edge_ips.sh
+++ mount_all_edge_ips.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sshfs pi@192.168.1.102: edge-pi1 -o nonempty
-sshfs pi@192.168.1.103: edge-pi2 -o nonempty
-sshfs pi@192.168.1.104: edge-pi3 -o nonempty
-sshfs pi@192.168.1.105: edge-pi4 -o nonempty
-sshfs pi@192.168.1.106: edge-pi5 -o nonempty
+sshfs pi@192.168.1.102: ~/edge-pi1 -o nonempty
+sshfs pi@192.168.1.103: ~/edge-pi2 -o nonempty
+sshfs pi@192.168.1.104: ~/edge-pi3 -o nonempty
+sshfs pi@192.168.1.105: ~/edge-pi4 -o nonempty
+sshfs pi@192.168.1.106: ~/edge-pi5 -o nonempty
