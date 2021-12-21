26bc148a57c14a5ff91c03fe3478964cf97deeb8
Simon Que <simon_justin_que@dlsu.edu.ph>
2021-12-21 11:35:17 +0800

    added convenience script to sync codes of all edge pis

---
+++ sync_all_edge_pi_codes.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+rsync -avzh BLAST-lab-data-collection-codes/ edge-pi1/BLAST-lab-data-collection-codes/
+rsync -avzh BLAST-lab-data-collection-codes/ edge-pi2/BLAST-lab-data-collection-codes/
+rsync -avzh BLAST-lab-data-collection-codes/ edge-pi3/BLAST-lab-data-collection-codes/
+rsync -avzh BLAST-lab-data-collection-codes/ edge-pi4/BLAST-lab-data-collection-codes/
+rsync -avzh BLAST-lab-data-collection-codes/ edge-pi5/BLAST-lab-data-collection-codes/
