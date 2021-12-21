bb000f310e2685a866ee4395ef520aa268edeb0c
Simon Que <simon_justin_que@dlsu.edu.ph>
2021-12-21 11:36:19 +0800

    added convenience script to sync codes of all edge pis

--- copy_latest_image_file.py
+++ copy_latest_image_file.py
@@ -3,6 +3,7 @@ from config import homedir, datadir, edgePiImagesSubDir
 
 edgeDirFormat = "edge-pi{i}/"
 
+os.system("rm -rf {cacheImagesPath}".format(cacheImagesPath = 'images/'))
 n = 5
 for i in range(1, n+1):
     currentEdgePiDir = homedir + edgeDirFormat.format(i=i) + edgePiImagesSubDir
@@ -11,6 +12,6 @@ for i in range(1, n+1):
         imageFilenames = os.listdir(currentEdgePiDir)
         imageFilenames.sort()
         latestImageFilename = imageFilenames[-1]
-        os.system("cp {srcImagePath} {destImagePath}".format(srcImagePath=currentEdgePiDir + latestImageFilename, destImagePath = 'images'))
+        os.system("cp {srcImagePath} {destImagePath}".format(srcImagePath=currentEdgePiDir + latestImageFilename, destImagePath = 'images/'))
     except:
         pass
