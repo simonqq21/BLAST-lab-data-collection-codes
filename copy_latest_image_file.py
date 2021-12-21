1ca46f3366f4b9f2a68bdd6023794429cd57b18c
simon <simon_justin_que@dlsu.edu.ph>
2021-12-21 12:17:50 +0800

    a

--- copy_latest_image_file.py
+++ copy_latest_image_file.py
@@ -1,12 +1,16 @@
 import os
-from config import homedir, datadir, edgePiImagesSubDir
+import shutil
+from config import homedir, datadir, edgePiImagesSubDir, edgePiImagesDirectory
 from config import create_path
 
 edgeDirFormat = "edge-pi{i}/"
 destDir = 'images/'
-create_path(destDir)
+destDirComplete = os.getcwd() + '/' + destDir
+create_path(edgePiImagesDirectory)
+print(edgePiImagesDirectory)
+#os.system("mkdir {destDirComplete}".format(destDirComplete=destDirComplete))
 
-os.system("rm -rf {cacheImagesPath}".format(cacheImagesPath = destDir))
+os.system("rm -rf {cacheImagesPath}".format(cacheImagesPath = edgePiImagesDirectory))
 n = 5
 for i in range(1, n+1):
     currentEdgePiDir = homedir + edgeDirFormat.format(i=i) + edgePiImagesSubDir
@@ -14,7 +18,10 @@ for i in range(1, n+1):
     try:
         imageFilenames = os.listdir(currentEdgePiDir)
         imageFilenames.sort()
-        latestImageFilename = imageFilenames[-1]
-        os.system("cp {srcImagePath} {destImagePath}".format(srcImagePath=currentEdgePiDir + latestImageFilename, destImagePath = destDir))
+        latestImageFilename = imageFilenames[-1]  
+        print(currentEdgePiDir + latestImageFilename)
+        print(destDirComplete + latestImageFilename)
+        shutil.copyfile(currentEdgePiDir + latestImageFilename, edgePiImagesDirectory + latestImageFilename)
+        #os.system("cp {srcImagePath} {destImagePath}".format(srcImagePath=currentEdgePiDir + latestImageFilename, destImagePath = destDirComplete + latestImageFilename))
     except:
         pass
