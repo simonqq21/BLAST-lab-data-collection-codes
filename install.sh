e48e398aed3bfe92ca5aa4556c065477676f1720
Simon Que <simon_justin_que@dlsu.edu.ph>
2022-04-26 21:50:49 +0800

    a

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@
 install_dir=/home/pi/BLAST_lab
 sudo apt-get update
 sudo apt-get install python3-pip apt-offline libatlas3-base i2c-tools
-mkdir -p install_dir
-sudo cp -r * install_dir
+mkdir -p $install_dir
+sudo cp -r * $install_dir
 sudo pip3 install -r requirements.txt
 sudo cp logger.service /lib/systemd/system
 sudo systemctl daemon-reload
